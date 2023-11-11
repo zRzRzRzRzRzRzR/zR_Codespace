540ca7190cadf337d3bae40f55aa2b165a18364e
zR <2448370773@qq.com>
2023-11-11 04:28:20 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -22,7 +22,7 @@ import PropTypes from "prop-types";
  */
 import image from "../images/wechat.jpg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText = "Yuxuan";
 
 const Home = ({ name, title }) => {
   return (
